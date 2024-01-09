3791ada8139bd00412ecbec07921224bb57888d4
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-01-09 10:20:11 -0300

    .

--- src/app.js
+++ src/app.js
@@ -14,6 +14,4 @@ app.use(express.json());
 app.use('/login', loginRoutes);
 app.use('/user', userRoutes);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
