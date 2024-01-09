9721f10eb51748fe6b78c867ff5617753a29119e
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-01-09 10:37:47 -0300

    req 7 - cria models category

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+// src/models/Category.js
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Category;
+};
+
